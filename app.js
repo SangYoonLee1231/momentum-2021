bffc06be2bd889e10b0e316b653b2737ba1192fb
SangYoonLee <sylagape1231@gmail.com>
2021-10-25 17:37:33 +0900

    Update app.js and index.html
    #3.4 Events part Two
    
    JavaScript로 HTML 문서를 가져와 수정하기

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const h1 = document.querySelector("div.hello:first-child h1")
+const title = document.querySelector(".hello h1");
+// const title = document.getElementById("hello");
 
-function handleTitleClick {
-    
+console.dir(title);
+
+// console.log(title);
+// title.style.color = "blue";
+
+function handleTitleClick() {
+  title.style.color = "blue";
+  console.log("title was clicked");
 }
 
-h1.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+  console.log("mouse is here!");
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+  console.log("mouse is gone!");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
