dc6745b66217179406a977dcc4de55cacd0bed1a
SangYoonLee <sylagape1231@gmail.com>
2021-11-01 16:27:47 +0900

    Update app.js
    End #4.2

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 */
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  /*
-  if (username === "") {
-    alert("Please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long!");
-  } else {
-    console.log("hello " + loginInput.value);
-  }
-  console.log("click");
-  */
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
