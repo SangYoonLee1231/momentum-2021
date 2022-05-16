4f6395ea9cb6701f29484f8cc94a944a9a273444
SangYoonLee <sylagape1231@gmail.com>
2022-05-16 22:28:59 +0900

    Update script.js
    2022-05-16 TIL, JS Momentum
    (~#2.4)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-const a = 5;
-const b = 2;
-let myName = "nico";
+const amIFat = null;
+let something;
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "nicolas";
-
-console.log("your new name is " + myName);
+console.log(amIFat);
+console.log(something, amIFat);
