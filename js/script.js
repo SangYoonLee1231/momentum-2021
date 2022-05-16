da62f7f56a6ef6257680f882bc75bf5629eee95f
SangYoonLee <sylagape1231@gmail.com>
2022-05-16 22:56:17 +0900

    Update script.js
    2022-05-16 Momentum
    (~#2.5)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const amIFat = null;
-let something;
+const daysOfWeek = ["mon", "tue", "wed", "thr", "fri", "sat"];
 
-console.log(amIFat);
-console.log(something, amIFat);
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+console.log(daysOfWeek);
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
