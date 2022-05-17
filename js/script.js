4cd2db8fb1b7365ace1d4b2cfe8891e48773f630
SangYoonLee <sylagape1231@gmail.com>
2022-05-18 03:42:00 +0900

    Update script.js
    2022-05-17 Momentum
    #2.8 Function pt.2 중간

--- js/script.js
+++ js/script.js
@@ -1,37 +1,11 @@
-//const player = ["nico", 121212, false, "little bit"];
-
-// Object
-const player = {
-  name: "nico",
-  points: 10,
-  fat: true,
-};
-
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-player.fat = false;
-console.log(player);
-
-//player = false;
-
-player.lastName = "potato";
-player.points += 15;
-console.log(player);
-
-// Function
-console.log("Hello my name is Nico");
-
-function sayHello(nameOfPerson, age) {
-  console.log(
-    "Hello my name is " + nameOfPerson + " and I'm " + age + " years old."
-  );
+function plus(firstNum, secondNum) {
+  console.log(firstNum + secondNum);
 }
 
-alert();
-console.log();
-sayHello("nico", 10);
-sayHello("dal", 15);
-sayHello("lee", 20);
-sayHello("lynn", 22);
+function divide(a, b) {
+  console.log(a / b);
+}
+
+plus(8, 16);
+plus(8, 60);
+divide(60, 8);
