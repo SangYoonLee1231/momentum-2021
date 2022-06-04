f402f85b7fdf0a1fa08aeab4b4905d992ee40983
SangYoonLee <sylagape1231@gmail.com>
2022-06-04 18:14:00 +0900

    Update app.js
    2022-06-04 Momentum
    (~3.4)

--- js/app.js
+++ js/app.js
@@ -7,8 +7,18 @@ title.innerText = "Click Me!";
 console.log(title);
 
 function handleTitleClick() {
-  console.log("Title was clicked!");
+  console.log("Title was clicked");
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
