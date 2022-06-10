16f37e6f2d018890577e887fe215023eb6e49aef
SangYoonLee <sylagape1231@gmail.com>
2022-06-10 17:49:53 +0900

    Update app.js
    2022-06-10 Momentum
    (~#3.5)

--- js/app.js
+++ js/app.js
@@ -19,6 +19,30 @@ function handleMouseLeave() {
   title.innerText = "Mouse is gone";
 }
 
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("You Copier");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOOD");
+}
+
 title.addEventListener("click", handleTitleClick);
+//title.onclick = handleTitleClick;
 title.addEventListener("mouseenter", handleMouseEnter);
+//title.onmouseenter = handleMouseEnter;
 title.addEventListener("mouseleave", handleMouseLeave);
+//title.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
