95c3b8b604d21b32aa0c0a6a454f8c4683309827
SangYoonLee <sylagape1231@gmail.com>
2022-06-19 00:54:40 +0900

    Update All
    2022-06-19 Momentum
    (~#3.7)

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ window.addEventListener("online", handleWindowOnline);
 
 const h1 = document.querySelector(".hello:first-child h1");
 
+/*
 function handleTitleClick() {
   const currentColor = h1.style.color;
   let newColor;
@@ -58,6 +59,16 @@ function handleTitleClick() {
     //console.log(h1.style.color);
   }
   h1.style.color = newColor;
+}
+*/
+
+function handleTitleClick() {
+  const clickedClass = "active";
+  if (h1.className === clickedClass) {
+    h1.className = "";
+  } else {
+    h1.className = clickedClass;
+  }
 }
 
 h1.addEventListener("click", handleTitleClick);
