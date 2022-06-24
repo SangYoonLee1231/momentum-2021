4de4aed47702defa6646f5fd3a2e54dc9e73f01d
SangYoonLee <sylagape1231@gmail.com>
2022-06-24 23:19:37 +0900

    Update All
    2022-06-24 Momentum
    (~#3.8 중)

--- js/app.js
+++ js/app.js
@@ -1,50 +1,3 @@
-/*
-const title = document.querySelector(".hello:first-child h1");
-
-title.innerText = "Click Me!";
-
-function handleTitleClick() {
-  console.log("Title was clicked");
-  title.style.color = "blue";
-}
-
-function handleMouseEnter() {
-  title.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("You Copier");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOOD");
-}
-
-title.addEventListener("click", handleTitleClick);
-//title.onclick = handleTitleClick;
-title.addEventListener("mouseenter", handleMouseEnter);
-//title.onmouseenter = handleMouseEnter;
-title.addEventListener("mouseleave", handleMouseLeave);
-//title.onmouseleave = handleMouseLeave;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-*/
-
 const h1 = document.querySelector(".hello:first-child h1");
 
 /*
@@ -61,13 +14,23 @@ function handleTitleClick() {
   h1.style.color = newColor;
 }
 */
-
+/*
 function handleTitleClick() {
   const clickedClass = "active";
   if (h1.className === clickedClass) {
     h1.className = "";
   } else {
     h1.className = clickedClass;
+  }
+}
+*/
+
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
   }
 }
 
