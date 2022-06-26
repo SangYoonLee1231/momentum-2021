0eb566175dacdda237afc636d7209069133ec8f3
SangYoonLee <sylagape1231@gmail.com>
2022-06-26 22:30:17 +0900

    Update All
    2022-06-26 momentum
    - (~#4.3)

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
-//const loginForm = document.querySelector("#login-form");
-//const loginInput = loginForm.querySelector("input");
-//const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
 
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const link = document.querySelector("a");
 
-function handleLoginBtnClick() {
-  /*
-  const username = loginInput.value;
-  if (username === "") {
-    alert("< Please Write Your Name >");
-  } else if (username.length > 15) {
-    alert("Your Name is TOO LONG");
-  } else {
-    alert(username);
-  }
-  */
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+function handleLinkClick(event) {
+  console.log(event);
+  event.preventDefault();
+  alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
