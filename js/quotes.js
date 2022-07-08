25182d9d72eac9ca49333d95ac5df791fd5f5a6e
SYL (SangYoonLee) <prestro1231@hufs.ac.kr>
2022-07-08 17:10:40 +0900

    Update All
    2022-07-08 momentum
    - (~#6.2)

--- js/quotes.js
+++ js/quotes.js
@@ -45,7 +45,9 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+const array_len = quotes.length
+const num = Math.floor(Math.random() * array_len);
+const todaysQuote = quotes[num];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
